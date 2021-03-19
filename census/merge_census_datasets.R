d13523221dd0541cd853e5130d0c600d240b98f7
fis1995 <fis@stanford.edu>
2021-03-19 10:45:05 -0500

    reshape census race data to long

--- census/merge_census_datasets.R
+++ census/merge_census_datasets.R
@@ -3,14 +3,15 @@
 # 
 # AUTHOR: FRANCINE STEPHENS
 # DATE CREATED: 3/4/21
-# LAST UPDATED: 3/14/21
+# LAST UPDATED: 3/19/21
 #-------------------------------------
 
-#SET-UP-------------------------------------------------------------------------
+# SET-UP------------------------------------------------------------------------
 
 ## LIBRARIES
 packages <- c(
-  "tidyverse"
+  "tidyverse", 
+  "panelr"
 )
 lapply(packages, library, character.only = T)
 
@@ -27,10 +28,11 @@ race00 <- readRDS(paste0(wd,
 race90 <- readRDS(paste0(wd,
                          "/race_90_on_10_cbg.rds"))
 
-#PREPARE KEY VARIABLE-----------------------------------------------------------
-## 1990 to 2010 GISJOIN in the NHGIS CW has an incorrect number of characters.
-## To fix, remove the 3rd character (a padded zero on the county identifier) & 
-## the 7th character (a padded zero on the tract identifier).
+# PROCESS VARIABLES & MERGE-----------------------------------------------------
+## 1990 PREPARATION INCLUDES FIXING KEY VARIABLE.
+  ## 1990 to 2010 GISJOIN in the NHGIS CW has an incorrect number of characters.
+  ## To fix, remove the 3rd character (a padded zero on the county identifier) & 
+  ## the 7th character (a padded zero on the tract identifier).
 race90_edited <- race90 %>%
   mutate(CBG_10 = str_replace_all(CBG_10, "G", ""),
          state = str_sub(CBG_10, end=2),
@@ -38,13 +40,39 @@ race90_edited <- race90 %>%
          tract = str_sub(CBG_10, start=8, end=13),
          cbg = str_sub(CBG_10, start=-1),
          GEOID = str_c(state, county, tract, cbg, sep="")) %>%
-  select(-CBG_10, -state:-cbg)
+  select(-CBG_10, -state:-cbg) %>%
+  rename_with(., ~ gsub("_90", "", .x)) %>%
+  mutate(wave = 1990) %>%
+  relocate(GEOID, wave) %>%
+  rename(OTHER_MULTIRACIAL_NH = "OTHER_RACE_NH",
+         ASIAN_PACIFIC_NH = "ASIAN_NH")
+
+race00_edited <- race00 %>%
+  rename_with(., ~ gsub("_00", "", .x)) %>%
+  rename(GEOID = "CBG_10",
+         ASIAN_PACIFIC_NH = "ASIAN_NH") %>%
+  mutate(wave = 2000,
+         OTHER_MULTIRACIAL_NH = OTHER_RACE_NH + MULTIRACIAL_NH) %>%
+  select(-OTHER_RACE_NH, -MULTIRACIAL_NH) %>%
+  relocate(OTHER_MULTIRACIAL_NH, .before = HISPANIC) %>%
+  relocate(GEOID, wave)
+
+race10_edited <- race10 %>%
+  rename(GEOID = "CBG_10") %>%
+  rename_with(., ~ gsub("_10", "", .x)) %>%
+  mutate(wave = 2010,
+         OTHER_MULTIRACIAL_NH = OTHER_RACE_NH + MULTIRACIAL_NH, 
+         ASIAN_PACIFIC_NH = ASIAN_NH + PACIFIC_NH) %>%
+  select(-OTHER_RACE_NH, -MULTIRACIAL_NH, -ASIAN_NH, -PACIFIC_NH) %>%
+  relocate(OTHER_MULTIRACIAL_NH, .before = HISPANIC) %>%
+  relocate(ASIAN_PACIFIC_NH, .after = AM_IND_NH) %>%
+  relocate(GEOID, wave)
 
 
-#JOIN ALL DATASETS--------------------------------------------------------------
-race_all_decades_cbg10 <- race10 %>%
-  left_join(., race00, by = "CBG_10") %>%
-  left_join(., race90_edited, by=c("CBG_10"="GEOID"))
+## APPEND ALL DATASETS
+race_all_decades_cbg10 <- rbind(race90_edited, race00_edited, race10_edited) %>%
+  arrange(GEOID, wave)
+
 
 # EXPORT
-saveRDS(race_all_decades_cbg10, "all_decades_race_on_cbg10.rds")
+saveRDS(race_all_decades_cbg10, "all_decades_race_on_cbg10_LONG.rds")
