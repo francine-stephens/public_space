5abfc8f68deb56838ec9e9d6eddd96003bd13c2a
fis1995 <fis@stanford.edu>
2021-03-22 15:24:53 -0500

    initialize SF slow streets data

---
+++ slow_streets/slow_street_geoprocessing.R
@@ -0,0 +1,65 @@
+#-------------------------------------
+# GEOPROCESSING
+# 
+# AUTHOR: FRANCINE STEPHENS
+# DATE CREATED: 3/21/21
+# LAST UPDATED: 3/21/21
+#-------------------------------------
+
+# SET-UP------------------------------------------------------------------------
+## LIBRARIES
+packages <- c(
+  "readr",
+  "tidyverse",
+  "sf",
+  "RSocrata",
+  "ggplot2",
+  "scales",
+  "ggcorrplot",
+  "tmap",
+  "tigris",
+  "censusapi", 
+  "tidycensus",
+  "corrplot",
+  "RColorBrewer"
+)
+lapply(packages, library, character.only = T)
+
+##APIs
+key <- "AIzaSyCztwSCIHyEKP_uyjPrdZKf3jgZFA-XV1Y"
+set_key(key=key)
+
+## PATHS
+setwd("~/Projects/public_space/slow_streets")
+wd <- getwd()
+#sf_shapes_path <- "C:/Users/Franc/Documents/Shapefile_Repository/san_francisco_shapes/"
+
+
+## DATA IMPORT
+ss_intersections <- read_csv(paste0(wd,
+                                "/sf_slow_streets.csv")
+                         )
+
+sf_street_intersections <- read.socrata(
+  "https://data.sfgov.org/resource/jfxm-zeee.json",
+  app_token = "EZnaKCxOX6zj0uyTkD340TWLh",
+  email     = "fis@stanford.edu",
+  password  = "SFdata2017"
+)
+
+sf_streets_csv <- read.socrata("https://data.sfgov.org/resource/3psu-pn9h.csv",
+                               app_token = "EZnaKCxOX6zj0uyTkD340TWLh",
+                               email     = "fis@stanford.edu",
+                               password  = "SFdata2017")
+
+# GEOPROCESSING-----------------------------------------------------------------
+## SPATIAL OBJECT
+streets_lines <- sf_streets_csv %>%
+         st_as_sf(., wkt = "line")
+
+
+## VISUALIZE
+ggplot() + 
+  geom_sf(data = streets_lines) #+ 
+  geom_polygon(data = )
+
