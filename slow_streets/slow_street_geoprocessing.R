a7789da7dbbddd1b6e6eb7bb06b5734a436e3bcc
fis1995 <fis@stanford.edu>
2021-03-27 16:27:46 -0500

    clean up slow streets geoprocessing code

--- slow_streets/slow_street_geoprocessing.R
+++ slow_streets/slow_street_geoprocessing.R
@@ -1,13 +1,14 @@
 #------------------------------------------------------------
-# GEOPROCESSING & CLEANING SLOW STREETS & NEIGHBORHOOD DATA
+# GEOPROCESSING &  SLOW STREETS & NEIGHBORHOOD DATA
 # 
 # AUTHOR: FRANCINE STEPHENS
 # DATE CREATED: 3/21/21
-# LAST UPDATED: 3/26/21
+# LAST UPDATED: 3/27/21
 #-------------------------------------------------------------
 
 # SET-UP------------------------------------------------------------------------
-## LIBRARIES
+
+#### LIBRARIES ####
 packages <- c(
   "readr",
   "tidyverse",
@@ -28,21 +29,24 @@ packages <- c(
 )
 lapply(packages, library, character.only = T)
 
-## PARAMETERS
+
+#### PARAMETERS ####
 socrata_token <- "EZnaKCxOX6zj0uyTkD340TWLh"
 email <- "fis@stanford.edu"
 pword <- "SFdata2017"
 wgs <- 4326
 proj_meters <- 7131   # epsg for SF city/county in meters (NAD) 
 
-## PATHS
+
+#### PATHS ####
 setwd("~/Projects/public_space/slow_streets")
 wd <- getwd()
 shapefiles_path <- "C:/Users/Franc/Documents/Shapefile_Repository/"
 sf_shapes_path <- "san_francisco_shapes/"
 sf_nhoods_path <- "analysis_nhoods_2010_census_tracts/"
 
-## DATA IMPORT
+
+#### INGEST DATA ####
 ss_intersections <- read_csv(paste0(wd,
                                     "/sf_slow_streets.csv")
                             )
@@ -65,24 +69,10 @@ sf_nhoods_tracts10 <- st_read(paste0(shapefiles_path,
                                      "geo_export_afde1190-a893-43d3-9611-1325253cf07f.shp")
                     )   
 
+
 # GEOPROCESSING-----------------------------------------------------------------
-## Process & Intersections (points)
-ss_intersections_sf <- ss_intersections %>% 
-  separate(intersection, c("st1", "st2"), sep = " & ") %>%
-  mutate(across(starts_with("st"), toupper)) %>% 
-  left_join(., sf_street_intersections, by = c("st1" = "street_name_1", 
-                                               "st2" = "street_name_2")
-  ) %>% 
-  select_at(vars(-contains("_coord"))) %>%
-  filter(id.y != "244828",
-         id.y != "250079") %>%
-  st_as_sf(., coords=c("longitude", "latitude"), crs = wgs)
 
-
-## Process street segments
-streets_lines <- sf_streets_csv %>%
-         st_as_sf(., wkt = "line")
-
+#### Process street segments #####
 all_ss_segments <- ss_intersections %>%
   filter(!is.na(street_segments)) %>%
   separate(intersection, c("st1", "st2"), sep = " & ") %>%
@@ -93,6 +83,8 @@ all_ss_segments <- ss_intersections %>%
   distinct(st1, segments, .keep_all = TRUE) %>%
   ungroup()
 
+streets_lines <- sf_streets_csv %>%
+         st_as_sf(., wkt = "line")
 
 ss_street_segments <- streets_lines %>%
   right_join(., all_ss_segments, by = c("streetname" = "st1","f_st" = "segments")) %>%
@@ -105,12 +97,20 @@ ss_street_segments <- streets_lines %>%
          length_miles = as.numeric(set_units(length_meters, mi))
          )
 
+ss_streets_lines <- ss_street_segments %>%
+  group_by(name) %>%
+  summarize(nblocks = n()) %>% 
+  st_set_crs(wgs) %>%
+  st_transform(., crs = proj_meters) %>%  
+  mutate(length_meters = st_length(.),
+         length_miles = as.numeric(set_units(length_meters, mi))
+  ) # Create multi-linestring object for mapping full slow street
 
 ss_street_segment_points <- st_cast(ss_street_segments, "POINT") %>%
-  distinct(line, .keep_all = TRUE)
+  distinct(line, .keep_all = TRUE)  # Get intersection points for isochrones
 
   
-  ## BUILD ISOCHRONES
+#### Construct Isochrones ####
   ss_10_min_walk_iso <- mb_isochrone(
     ss_street_segment_points,
     profile = "walking",
@@ -124,6 +124,7 @@ ss_street_segment_points <- st_cast(ss_street_segments, "POINT") %>%
     group_by(name) %>% 
     summarize()
   
+  ## Visualize ##
   leaflet() %>% 
     addMapboxTiles(
       style_id = "streets-v11",
@@ -141,7 +142,7 @@ ss_street_segment_points <- st_cast(ss_street_segments, "POINT") %>%
     ) 
   
   
-## Aggregations of street segments
+#### Aggregate street segments ####
 ss_length_table <- ss_street_segments %>%
   group_by(name) %>%
   summarize(across(starts_with("length"), ~sum(.x, na.rm = TRUE))) %>%
@@ -162,36 +163,24 @@ ss_length_by_nhood <- ss_length_by_nhood_ss %>%
   arrange(-length_meters)
 
 
-## Create multi-line string object
-ss_streets_lines <- ss_street_segments %>%
-  group_by(name) %>%
-  summarize(nblocks = n()) %>% 
-  st_set_crs(wgs) %>%
-  st_transform(., crs = proj_meters) %>%  
-  mutate(length_meters = st_length(.),
-         length_miles = as.numeric(set_units(length_meters, mi))
-        )
+  #### Visualize ####
+  leaflet() %>% 
+    addMapboxTiles(
+      style_id = "streets-v11",
+      username = "mapbox"
+    ) %>%
+    addCircleMarkers(
+      data = ss_intersections_sf,
+      radius = 1,
+      label = ~name, 
+    ) %>% 
+    addPolylines(
+      data = ss_streets_lines %>% st_transform(., crs = wgs)
+    )
 
-
-## VISUALIZATIONS
-leaflet() %>% 
-  addMapboxTiles(
-    style_id = "streets-v11",
-    username = "mapbox"
-  ) %>%
-  addCircleMarkers(
-    data = ss_intersections_sf,
-    radius = 1,
-    label = ~name, 
-  ) %>% 
-  addPolylines(
-    data = ss_streets_lines %>% st_transform(., crs = wgs)
-  )
-
-
-ggplot() + 
-  geom_sf(data = streets_lines) #+ 
-  geom_polygon(data = )
+  ggplot() + 
+    geom_sf(data = streets_lines) #+ 
+    geom_polygon(data = )
 
 
 ## NEIGHBORHOODS 
@@ -202,21 +191,3 @@ sf_nhoods <- sf_nhoods_tracts10 %>%
   st_transform(., crs = st_crs(proj_meters))
 
 
-
-
-leaflet() %>%
-  addMapboxTiles(
-    style_id = "streets-v11",
-    username = "mapbox"
-  ) %>%
-  addPolygons(
-    data = sf_zillow_nhoods %>% st_transform(., crs = wgs),
-    color = "red",
-    weight = 2,
-    label = ~name
-  ) %>% 
-  addPolylines(
-    data = ss_streets_lines %>% st_transform(., crs = wgs),
-    color = "blue",
-    label = ~name
-  )
