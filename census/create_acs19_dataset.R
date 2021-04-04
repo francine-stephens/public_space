11f612042fdbe4562db6e6c83bbd6c4004fb9238
fis1995 <fis@stanford.edu>
2021-04-03 23:22:18 -0500

    ACS19 housing and income data created
    creates variables for ACS19 housing and income to be merged to rest of demographic and green space dataframe.

--- census/create_acs19_dataset.R
+++ census/create_acs19_dataset.R
@@ -15,7 +15,8 @@ lapply(packages, library, character.only = T)
 ## PATHS
 setwd("~/Projects/public_space/census")
 wd <- getwd()
-acs_path <- "/nhgisacs2019_csv/nhgis0022_ds244_20195_2019_blck_grp.csv"
+acs_race_path <- "/nhgisacs2019_csv/nhgis0022_ds244_20195_2019_blck_grp.csv"
+acs_hu_inc_path <- "/nhgisacs2019_csv/R12793886_SL150.csv"
 
 ## PARAMETERS
 selected_vars <- c("GISJOIN",
@@ -46,12 +47,35 @@ renamed_vars <- c("GISJOIN",
                   "HISPANIC"
 )
 
+renamed_hu_inc_vars <- c("GEOID",
+                         "HH",
+                         "INC_LT10K",
+                         "INC_10KTO14999",
+                         "INC_15KTO19999",
+                         "INC_20KTO24999",
+                         "INC_25KTO29999",
+                         "INC_30KTO34999",
+                         "INC_35KTO39999",
+                         "INC_40KTO44999",
+                         "INC_45KTO49999",
+                         "INC_50KTO59999",
+                         "INC_60KTO74999",
+                         "INC_75KTO99999",
+                         "INC_100KTO124999",
+                         "INC_125KTO149999",
+                         "INC_150KTO199999",
+                         "INC_200KPLUS",
+                         "H_UNITS",
+                         "OCCUPIED_UNITS",
+                         "OWNER_OCC",
+                         "RENTER_OCC",
+                         "VACANT_UNITS")
 
 # CLEAN & PROCESS DATA----------------------------------------------------------
-acs <- read_csv(paste0(wd, 
-                       acs_path))
+acs_race <- read_csv(paste0(wd, 
+                       acs_race_path))
 
-acs_reduced <- acs %>%
+acs_race_reduced <- acs_race %>%
   select(all_of(selected_vars)) %>%
   rename_at(vars(GISJOIN:ALUKE012), ~ renamed_vars) %>%
   mutate(wave = str_remove(wave, "2015-"),
@@ -63,6 +87,21 @@ acs_reduced <- acs %>%
   relocate(ASIAN_PACIFIC_NH, .after = AM_IND_NH) %>%
   relocate(GEOID, wave)
 
+acs_hu_inc <- read_csv(paste0(wd, 
+                              acs_hu_inc_path))
+
+acs_hu_inc_reduced <- acs_hu_inc %>%
+  select(Geo_GEOID,
+         SE_A14001_001:SE_A10001_001,
+         SE_A10060_001:SE_A10060_003,
+         SE_A10044_003) %>%
+  rename_at(vars(Geo_GEOID:SE_A10044_003), ~ renamed_hu_inc_vars) %>%
+  mutate(wave = "2019",
+         GEOID = str_sub(GEOID, start = 8)
+         ) %>%
+  relocate(GEOID, wave)
+
 
 # EXPORTS-----------------------------------------------------------------------
-saveRDS(acs_reduced, "cbg_acs_2019_race.rds")
+saveRDS(acs_race_reduced, "cbg_acs_2019_race.rds")
+saveRDS(acs_hu_inc_reduced, "cbg_acs_2019_hu_inc.rds")
